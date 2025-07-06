2c985766a7ec24e3f274b6e494143fb952bf1375
Юнжаков Вячеслав <slava-shalala@mail.ru>
2025-07-06 23:16:06 +0300

    button size fix

--- TMessagesProj/src/main/java/org/telegramIunzhakov/ui/ProfileButton.java
+++ TMessagesProj/src/main/java/org/telegramIunzhakov/ui/ProfileButton.java
@@ -1,6 +1,8 @@
 package org.telegramIunzhakov.ui;
 
 
+import static org.telegramIunzhakov.messenger.AndroidUtilities.dp;
+
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
@@ -23,7 +25,7 @@ public class ProfileButton extends FrameLayout {
 
         GradientDrawable background = new GradientDrawable();
         background.setShape(GradientDrawable.RECTANGLE);
-        background.setCornerRadius(dpToPx(context, 8));
+        background.setCornerRadius(dp(8));
         background.setColor(Theme.isCurrentThemeDark() || Theme.isCurrentThemeNight() ? 0xff597590 : 0xff0E477F);
         background.setAlpha(64);
         setBackground(background);
@@ -31,7 +33,7 @@ public class ProfileButton extends FrameLayout {
         // Внутренний вертикальный контейнер
         LinearLayout innerLayout = new LinearLayout(context);
         innerLayout.setOrientation(LinearLayout.VERTICAL);
-        innerLayout.setGravity(Gravity.CENTER);
+        innerLayout.setGravity(Gravity.TOP);
 
         LayoutParams innerParams = new LayoutParams(
                 LayoutParams.WRAP_CONTENT,
@@ -44,8 +46,8 @@ public class ProfileButton extends FrameLayout {
         icon.setImageResource(data.getImageResId());
         icon.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
         LinearLayout.LayoutParams iconParams = new LinearLayout.LayoutParams(
-                dpToPx(context, 48),
-                dpToPx(context, 48)
+                dp(24),
+                dp(24)
         );
         iconParams.gravity = Gravity.TOP|Gravity.CENTER_HORIZONTAL;
         innerLayout.addView(icon, iconParams);
@@ -53,27 +55,19 @@ public class ProfileButton extends FrameLayout {
         SimpleTextView textView = new SimpleTextView(context);
         textView.setText(data.getText());
         textView.setTextSize(12);
-        textView.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL);
+        textView.setGravity(Gravity.CENTER_HORIZONTAL);
         textView.setTextColor(Color.WHITE);
         LinearLayout.LayoutParams textParams = new LinearLayout.LayoutParams(
                 LayoutParams.WRAP_CONTENT,
                 LayoutParams.WRAP_CONTENT
         );
-        textParams.topMargin = dpToPx(context, 2);
+        textParams.topMargin = dp(2);
         textParams.gravity = Gravity.CENTER;
         innerLayout.addView(textView, textParams);
         setOnClickListener(data.getClickListener());
 
-        int padding = dpToPx(context, 4);
-        setPadding(padding, padding, padding, padding);
-    }
-
-    private int dpToPx(Context context, float dp) {
-        return (int) TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP,
-                dp,
-                context.getResources().getDisplayMetrics()
-        );
+        int padding = dp(8);
+        setPadding(0, padding, 0, padding);
     }
 }
 
